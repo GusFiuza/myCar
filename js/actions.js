6cb3d989ccd54dbd35240a532737fa958c8cb5e2
Gustavo Fiuza <gfiuza@gmail.com>
2024-01-31 10:11:40 -0300

    navegation

--- js/actions.js
+++ js/actions.js
@@ -1,4 +1,5 @@
 let APIData
+let currentPosition
 let requestHTTP = new XMLHttpRequest()
 let link = `https://6588253990fa4d3dabf982d3.mockapi.io/onix`
 
@@ -7,40 +8,9 @@ function getDataFromAPI() {
     requestHTTP.onload = function (e) {
         if (requestHTTP.readyState === 4 & requestHTTP.status === 200) {
             APIData = JSON.parse(requestHTTP.responseText)
+            currentPosition = APIData.length - 1
             if (APIData.length != 0) {
-                for (i = 0; i < APIData.length; i++) {
-                    document.getElementById("l2").textContent = "Data: " + APIData[i].Date
-                    document.getElementById("l3").textContent = "Odômetro: " + APIData[i].Odometer.toLocaleString('pt-BR', { style: 'decimal' })
-                    document.getElementById("l4").textContent = "Valor do litro: " + APIData[i].Price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-                    document.getElementById("l5").textContent = APIData[i].Liters.toFixed(2).toLocaleString().replace('.', ',') + " litros"
-                    if (i > 0) {
-                        day = parseInt(APIData[i-1].Date.substring(0,2)) + 1
-                        if (day < 10) {
-                            day = '0' + day.toString()
-                        } else {
-                            day = day.toString()
-                        }
-                        month = APIData[i-1].Date.substring(3,5)
-                        year = APIData[i-1].Date.substring(6,10)
-                        dateOne = new Date(year + '-' + month + '-' + day)
-
-                        day = parseInt(APIData[i].Date.substring(0,2)) + 1
-                        if (day < 10) {
-                            day = '0' + day.toString()
-                        } else {
-                            day = day.toString()
-                        }
-                        month = APIData[i].Date.substring(3,5)
-                        year = APIData[i].Date.substring(6,10)
-                        dateTwo = new Date(year + '-' + month + '-' + day)
-                        
-                        timeDiff = Math.abs(dateTwo.getTime() - dateOne.getTime());
-                        diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-                        
-                        document.getElementById("l6").textContent = "Consumo: " + ((APIData[i].Odometer-APIData[i-1].Odometer)/APIData[i].Liters).toFixed(2) + " km/l"
-                        document.getElementById("l7").textContent = "Média de " + ((APIData[i].Odometer-APIData[i-1].Odometer)/diffDays).toFixed() + "km por dia"
-                    }
-                }
+                 fillView(currentPosition)
             } else {
                 document.head.title = "Fulano de Tal";
             }
@@ -54,6 +24,55 @@ function getDataFromAPI() {
     requestHTTP.send()
 }
 
+function fillView(position) {
+    document.getElementById("l1").textContent = "Data: " + APIData[position].Date
+    document.getElementById("l2").textContent = "Odômetro: " + APIData[position].Odometer.toLocaleString('pt-BR', { style: 'decimal' })
+    document.getElementById("l3").textContent = "Valor do litro: " + APIData[position].Price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    document.getElementById("l4").textContent = APIData[position].Liters.toFixed(2).toLocaleString().replace('.', ',') + " litros"
+    if (position == 0) {
+        document.getElementById("l1").textContent = document.getElementById("l1").textContent + " (Primeiro)"
+        document.getElementById("l5").textContent = ""
+        document.getElementById("l6").textContent = ""
+    } 
+    if (position == APIData.length -1) {
+        document.getElementById("l1").textContent = document.getElementById("l1").textContent + " (Último)"
+    }
+    if (position > 0) {
+        day = parseInt(APIData[position-1].Date.substring(0,2)) + 1
+        if (day < 10) {
+            day = '0' + day.toString()
+        } else {
+            day = day.toString()
+        }
+        month = APIData[position-1].Date.substring(3,5)
+        year = APIData[position-1].Date.substring(6,10)
+        dateOne = new Date(year + '-' + month + '-' + day)
+
+        day = parseInt(APIData[position].Date.substring(0,2)) + 1
+        if (day < 10) {
+            day = '0' + day.toString()
+        } else {
+            day = day.toString()
+        }
+        month = APIData[position].Date.substring(3,5)
+        year = APIData[position].Date.substring(6,10)
+        dateTwo = new Date(year + '-' + month + '-' + day)
+        
+        timeDiff = Math.abs(dateTwo.getTime() - dateOne.getTime());
+        diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        
+        document.getElementById("l5").textContent = "Consumo: " + ((APIData[position].Odometer-APIData[position-1].Odometer)/APIData[position].Liters).toFixed(2) + " km/l"
+        document.getElementById("l6").textContent = "Média de " + ((APIData[position].Odometer-APIData[position-1].Odometer)/diffDays).toFixed() + "km por dia"
+    }
+}
+
+function previousData() {
+    if (currentPosition > 0) {
+        currentPosition--
+        fillView(currentPosition)
+    }
+}
+
 function editData() {
     if ( document.getElementById('view').hidden ) {
         document.getElementById('edit').setAttribute('hidden', '')
@@ -65,6 +84,13 @@ function editData() {
         document.getElementById('edit').children[1].value = today.toLocaleDateString("pt-BR")
         document.getElementById('edit').children[3].value = APIData[APIData.length-1].Odometer.toLocaleString('pt-BR', { style: 'decimal' })
         document.getElementById('edit').children[5].value = APIData[APIData.length-1].Price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    }
+}
+
+function nextData() {
+    if (currentPosition < APIData.length-1) {
+        currentPosition++
+        fillView(currentPosition)
     }
 }
 
