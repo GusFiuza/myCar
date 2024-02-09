82df8123408430889b38e4be6aabfa298ee3d26b
Gustavo Fiuza <gfiuza@gmail.com>
2024-02-09 17:22:36 -0300

    Using new API

--- js/actions.js
+++ js/actions.js
@@ -1,88 +1,88 @@
-let APIData
+const projectToken = '65bc2b4f52189914b5bda99c'
+let FuelingData
 let currentPosition
-let requestHTTP = new XMLHttpRequest()
-let link = `https://6588253990fa4d3dabf982d3.mockapi.io/onix`
 
-// FIPE data
-let FIFEData
-FIPEData = JSON.parse('[ \
-                    {"month":202301,"value":99292}, \
-                    {"month":202302,"value":99212}, \
-                    {"month":202303,"value":99685}, \
-                    {"month":202304,"value":99040}, \
-                    {"month":202305,"value":99798}, \
-                    {"month":202306,"value":100430}, \
-                    {"month":202307,"value":99899}, \
-                    {"month":202308,"value":98835}, \
-                    {"month":202309,"value":98575}, \
-                    {"month":202310,"value":97692}, \
-                    {"month":202311,"value":97551}, \
-                    {"month":202312,"value":97526}, \
-                    {"month":202401,"value":96211} \
-                ]')
 
-function getDataFromAPI() {
-    requestHTTP.open('GET', link, true)
-    requestHTTP.onload = function (e) {
-        if (requestHTTP.readyState === 4 & requestHTTP.status === 200) {
-            APIData = JSON.parse(requestHTTP.responseText)
-            currentPosition = APIData.length - 1
-            if (APIData.length != 0) {
-                 fillView(currentPosition)
-            } else {
-                document.head.title = "Fulano de Tal";
+function testing(carId) {
+    console.log(carId)
+}
+
+function getDataFromAPI(carId) {
+    getCarData(carId)
+    getFuelingData(carId)
+}
+
+function getCarData(carId) {
+    fetch(`https://${projectToken}.mockapi.io/car/${carId}`, {
+        method: 'GET',
+        headers: {'content-type':'application/json'},
+      }).then(res => {
+        if (res.ok) {return res.json()}
+      }).then(tasks => {
+        document.body.children[0].children[0].textContent = tasks.brand
+        document.body.children[0].children[1].textContent = tasks.model
+        document.body.children[0].children[2].textContent = tasks.version
+      }).catch(error => {})
+}
+
+function getFuelingData(carId) {
+    fetch(`https://${projectToken}.mockapi.io/car/${carId}/fueling`, {
+        method: 'GET',
+        headers: {'content-type':'application/json'},
+      }).then(res => {
+        if (res.ok) {return res.json()}
+      }).then(tasks => {
+        for (i = 0; i < tasks.length; i++) {
+            FuelingData = tasks
+            currentPosition = FuelingData.length - 1
+            if (FuelingData.length != 0) {
+                fillView(currentPosition)
             }
-        } else {
-            console.log("Diferente de 4 e 200...")
         }
-    }
-    requestHTTP.onerror = function (e) {
-        console.log("Erro...");
-    }
-    requestHTTP.send()
+      }).catch(error => {})
 }
 
 function fillView(position) {
-    document.getElementById("l1").textContent = "Data: " + APIData[position].Date
-    document.getElementById("l2").textContent = "Odômetro: " + APIData[position].Odometer.toLocaleString('pt-BR', { style: 'decimal' })
-    document.getElementById("l3").textContent = "Valor do litro: " + APIData[position].Price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-    document.getElementById("l4").textContent = APIData[position].Liters.toFixed(2).toLocaleString().replace('.', ',') + " litros"
+    document.getElementById("l1").textContent = "Data: " + FuelingData[position].date
+    document.getElementById("l2").textContent = "Odômetro: " + FuelingData[position].odometer.toLocaleString('pt-BR', { style: 'decimal' })
+    document.getElementById("l3").textContent = "Valor do litro: " + FuelingData[position].price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    document.getElementById("l4").textContent = FuelingData[position].liters.toFixed(2).toLocaleString().replace('.', ',') + " litros"
     if (position == 0) {
         document.getElementById("l1").textContent = document.getElementById("l1").textContent + " (Primeiro)"
         document.getElementById("l5").textContent = ""
         document.getElementById("l6").textContent = ""
     } 
-    if (position == APIData.length -1) {
+    if (position == FuelingData.length -1) {
         document.getElementById("l1").textContent = document.getElementById("l1").textContent + " (Último)"
     }
     if (position > 0) {
-        day = parseInt(APIData[position-1].Date.substring(0,2))
+        day = parseInt(FuelingData[position-1].date.substring(0,2))
         if (day < 10) {
             day = '0' + day.toString()
         } else {
             day = day.toString()
         }
-        month = APIData[position-1].Date.substring(3,5)
-        year = APIData[position-1].Date.substring(6,10)
+        month = FuelingData[position-1].date.substring(3,5)
+        year = FuelingData[position-1].date.substring(6,10)
         dateOne = new Date(year + '-' + month + '-' + day)
         dateOne.setDate(dateOne.getDate() + 1)
 
-        day = parseInt(APIData[position].Date.substring(0,2))
+        day = parseInt(FuelingData[position].date.substring(0,2))
         if (day < 10) {
             day = '0' + day.toString()
         } else {
             day = day.toString()
         }
-        month = APIData[position].Date.substring(3,5)
-        year = APIData[position].Date.substring(6,10)
+        month = FuelingData[position].date.substring(3,5)
+        year = FuelingData[position].date.substring(6,10)
         dateTwo = new Date(year + '-' + month + '-' + day)
         dateTwo.setDate(dateTwo.getDate() + 1)
 
         timeDiff = Math.abs(dateTwo.getTime() - dateOne.getTime());
         diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
         
-        document.getElementById("l5").textContent = "Consumo: " + ((APIData[position].Odometer-APIData[position-1].Odometer)/APIData[position].Liters).toFixed(2) + " km/l"
-        document.getElementById("l6").textContent = "Média de " + ((APIData[position].Odometer-APIData[position-1].Odometer)/diffDays).toFixed() + "km por dia"
+        document.getElementById("l5").textContent = "Consumo: " + ((FuelingData[position].odometer-FuelingData[position-1].odometer)/FuelingData[position].liters).toFixed(2) + " km/l"
+        document.getElementById("l6").textContent = "Média de " + ((FuelingData[position].odometer-FuelingData[position-1].odometer)/diffDays).toFixed() + "km por dia"
     }
 }
 
@@ -98,39 +98,37 @@ function editData() {
     document.getElementById('edit').removeAttribute('hidden')
     today = new Date()
     document.getElementById('edit').children[1].value = today.toLocaleDateString("pt-BR")
-    document.getElementById('edit').children[3].value = APIData[APIData.length-1].Odometer.toLocaleString('pt-BR', { style: 'decimal' })
-    document.getElementById('edit').children[5].value = APIData[APIData.length-1].Price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    document.getElementById('edit').children[3].value = FuelingData[FuelingData.length-1].odometer.toLocaleString('pt-BR', { style: 'decimal' })
+    document.getElementById('edit').children[5].value = FuelingData[FuelingData.length-1].price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 }
 
 function nextData() {
-    if (currentPosition < APIData.length-1) {
+    if (currentPosition < FuelingData.length-1) {
         currentPosition++
         fillView(currentPosition)
     }
 }
 
-function confirmData() {
+function confirmData(carId) {
     const onixData = {
-        Date: `${document.getElementById('edit').children[1].value}`,
-        Odometer: parseInt(document.getElementById('edit').children[3].value.replace('.','')),
-        Price: parseFloat(document.getElementById('edit').children[5].value.replace('R$','').replace(',','.')),
-        Liters: parseFloat(document.getElementById('edit').children[7].value.replace(',','.'))
+        date: `${document.getElementById('edit').children[1].value}`,
+        odometer: parseInt(document.getElementById('edit').children[3].value.replace('.','')),
+        price: parseFloat(document.getElementById('edit').children[5].value.replace('R$','').replace(',','.')),
+        liters: parseFloat(document.getElementById('edit').children[7].value.replace(',','.')),
+        carId: parseInt(carId)
     };
-      
-    fetch('https://6588253990fa4d3dabf982d3.mockapi.io/onix', {
+
+    fetch(`https://${projectToken}.mockapi.io/car/${carId}/fueling`, {
         method: 'POST',
         headers: {'content-type':'application/json'},
-        // Send your data in the request body as JSON
         body: JSON.stringify(onixData)
     }).then(res => {
             if (res.ok) {
-                getDataFromAPI()
+                return res.json()
             }
-        // handle error
         }).then(task => {
-        // do something with the new task
+            getDataFromAPI(1)
         }).catch(error => {
-        // handle error
     })
     cancelData()
 
@@ -139,8 +137,4 @@ function confirmData() {
 function cancelData() {
     document.getElementById('edit').setAttribute('hidden', '')
     document.getElementById('view').removeAttribute('hidden')
-}
-
-function testing() {
-    console.log(FIPEData[FIPEData.length-1].value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))
 }
